e6a10095fb24629dfb7c8d5bbaea9120a0730339
Tatiana-E <tanya.era1989@gmail.com>
2024-07-30 18:46:50 +0500

    Add files via upload

---
+++ Okna.py
@@ -0,0 +1,57 @@
+def get_values():
+    n1 = int(number1_entry.get())
+    n2 = int(number2_entry.get())
+    return n1, n2
+def insert_values(value):
+    answer_entry.delete(0, 'end')
+    answer_entry.insert(0, value)
+
+def add():
+    n1, n2 = get_values()
+    res = n1 + n2
+    insert_values(res)
+def sub():
+    n1, n2 = get_values()
+    res = n1 - n2
+    insert_values(res)
+def dif():
+    n1, n2 = get_values()
+    res = n1 / n2
+    insert_values(res)
+def mul():
+    n1, n2 = get_values()
+    res = n1 * n2
+    insert_values(res)
+
+import tkinter as tk
+
+
+window = tk.Tk()
+window.title('Калькулятор')
+window.geometry("350x350")
+window.resizable(False,False)
+button_add = tk.Button(window, text= '+', width=2, height=2, command=add)
+button_add.place (x=100, y=200)
+button_sub = tk.Button(window, text= '-', width=2, height=2, command=sub)
+button_sub.place (x=150, y=200)
+button_mul = tk.Button(window, text= '*', width=2, height=2, command=mul)
+button_mul.place (x=200, y=200)
+button_dif = tk.Button(window, text= '/', width=2, height=2, command=dif)
+button_dif.place (x=250, y=200)
+number1_entry = tk.Entry(window, width=28)
+number1_entry.place(x=100, y=75)
+number2_entry = tk.Entry(window, width=28)
+number2_entry.place(x=100, y=150)
+answer_entry = tk.Entry(window, width=28)
+answer_entry.place(x=100, y=300)
+number1 = tk.Label(window, text='Введите первое число')
+number1.place(x=100, y=50)
+number2 = tk.Label(window, text='Введите второе число')
+number2.place(x=100, y=125)
+answer = tk.Label(window, text='Ответ')
+answer.place(x=100, y=275)
+
+
+window.mainloop()
+
+
